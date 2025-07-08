cb685e7f351d127cc557d5bb2e7ceb6f1d10ccd8
eliezer <eliezermasiala2000@gmail.com>
2025-07-08 23:59:56 +0100

    fix: resolve heroImg import path issue in deployment

--- src/components/HeroImage/index.jsx
+++ src/components/HeroImage/index.jsx
@@ -11,7 +11,12 @@ function HeroImage () {
     const { theme } = useContext(ThemeContext);
 
     return (
-        <section className="relative w-full h-[600px] bg-cover bg-center rounded-md" style={{ backgroundImage: `url(${imghero})` }} >
+       
+        <section className="relative capitalize">
+            <div>
+                <img src={imghero} className="w-full h-[450px] object-cover rounded-md" alt="hero image"/>
+            </div>
+        
             <div className={`absolute -bottom-8 md:-bottom-12 left-1/2 md:left-[40%] lg:left-[30%] xl:left-[25%] ${theme === 'dark' ? 'bg-[#181A2A] border-[#242535] text-white ' : 'bg-white border-[#E8E8EA] text-[#181A2A]'}
                 transform -translate-x-1/2 border rounded-md px-4 py-6 shadow-lg w-[90%] sm:w-[80%] md:w-[70%] lg:w-[50%] xl:w-[32rem]`}
             >
