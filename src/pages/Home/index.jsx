edbf46793904a3e9d48e6c69b19e7e3d2a0d2bc0
eliezer <eliezermasiala2000@gmail.com>
2025-11-26 08:42:51 +0100

    fatal: home page load more

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 // eslint-disable-next-line no-unused-vars
 import { motion } from 'framer-motion';
 import HeroImage from '../../components/HeroImage';
@@ -10,16 +10,42 @@ import { ThemeContext } from '../../utils/Context/ThemeContext';
 function Home() {
   
   const { theme } = useContext(ThemeContext);
-  const apiUrl = `${import.meta.env.VITE_API_URL}/posts?_limit=21`;
+  const limit = 6;
+  const [page,setPage] = useState(1);
+  const [posts, setPosts] = useState([]);
+  const [initialLoading, setInitialLoading] = useState(false);
+
+  const apiUrl = `${import.meta.env.VITE_API_URL}/posts?_limit=${limit}&_page=${page}`;
   const apiUrlUsers = `${import.meta.env.VITE_API_URL}/users`;
   
   const {data:users} = useFetch(apiUrlUsers);
-  const {data, loading, error} = useFetch(apiUrl);
-  const postsWithAuthor = usePostsWithAuthor(data, users);
+  const {data: pageData, loading, error} = useFetch(apiUrl);
+  const postsWithAuthor = usePostsWithAuthor(pageData, users);
+
+  useEffect(() => {
+    if(Array.isArray(pageData) && pageData.length > 0) {
+      setPosts((prev) => {
+        const existingIds = new Set(prev.map(p => p.id));
+        const newPosts =  postsWithAuthor.filter(p => !existingIds.has(p.id));
+        return [...prev, ...newPosts];
+      });
+      setInitialLoading(true);
+    }else if (!pageData && !loading && !initialLoading) {
+      setInitialLoading(true);
+    }
+  },[pageData,postsWithAuthor, loading, initialLoading]);
+
+  const hasMore = Array.isArray(pageData)? pageData.length === limit : false ;
+  const handleLoadMore = () => {
+    if(hasMore && !loading)
+    {
+      setPage((prev) => prev + 1);
+    }
+  }
 
   return (
     <div>
-      {loading && <div class="flex items-center justify-center h-screen">
+      {!initialLoading && loading && <div class="flex items-center justify-center h-screen">
         <motion.div
           animate={{ rotate: 360 }}
           transition={{
@@ -33,10 +59,20 @@ function Home() {
         />
         </div>
       }
-      {!loading && !error && (
+
+      {initialLoading && !error && (
         <>
           <HeroImage />
-          <Lastpost data={postsWithAuthor} />
+          <Lastpost data={posts} />
+          <div className="flex justify-center my-8">
+            { hasMore ? (
+                <button onClick={handleLoadMore} className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition">
+                  { loading ? 'chargement..' : 'load more'}
+                </button>
+            ) : (
+              <p>No more posts to load.</p>
+            )}
+          </div>
         </>
       )}
       
